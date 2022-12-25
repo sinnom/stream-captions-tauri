250cbc7a25dec4bdaf69f0b7b946a7375d52e93e
sinnom <106713618+sinnom@users.noreply.github.com>
2022-12-24 20:18:28 -0600

    Restructure app around two main windows

---
+++ src/ControlsRoot.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>
+      This is the controls
+    </div>
+  </React.StrictMode>
+);
