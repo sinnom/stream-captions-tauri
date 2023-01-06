e135ece9d64d6a8b6956c0b81ccbe987db953ab3
sinnom <106713618+sinnom@users.noreply.github.com>
2023-01-05 18:42:39 -0600

    Add sound capability to rust

--- src/display/DisplayTextAudio.tsx
+++ src/display/DisplayTextAudio.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import soundByte from '../assets/twoTone2.mp3';
 import { Howl } from 'howler';
+import { invoke } from '@tauri-apps/api';
 
 let sound = new Howl({
   src: [soundByte],
@@ -16,8 +17,10 @@ export interface IDisplayTextAudioProps {
 export function DisplayTextAudio(props: IDisplayTextAudioProps) {
   useEffect(() => {
     if (props.text !== '' && !props.text.endsWith(' ')) {
-      const instance = sound.play();
-      setTimeout(() => sound.stop(instance), props.soundDuration);
+      // const instance = sound.play();
+      // setTimeout(() => sound.stop(instance), props.soundDuration);
+      invoke('sound', { soundFile: soundByte });
+      console.log("invoked");
     }
     return () => { sound.stop() };
   }, [props.text]);
